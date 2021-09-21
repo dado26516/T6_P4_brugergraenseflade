da1707bb67137f4d334fa23ee133bb5e39a0965a
TheRealArtonn <61601482+TheRealArtonn@users.noreply.github.com>
2021-09-21 20:53:27 +0200

    css og js

--- js/arton.js
+++ js/arton.js
@@ -1,5 +1,25 @@
+const mainInput = document.getElementById('main-input');
 
-document.getElementById('main-input').addEventListener('input', function(e) {
-    document.getElementById('main-search').classList.add('dropdown__module--dropped');
-    document.getElementById('main-dropdown').style.display = "flex";
+
+document.getElementById('main-overlay').style.display = "none";
+
+document.getElementById('page-input').addEventListener('click', function(e) {
+    document.getElementById('main-overlay').style.display = "block";
+});
+
+mainInput.addEventListener('input', function(e) {
+    
+    let mainInputValue = mainInput.value;
+    
+    console.log(mainInputValue);
+    
+    if(mainInputValue == "Fablab") {
+        document.getElementById('main-search').classList.add('dropdown__module--dropped');
+        document.getElementById('main-dropdown').style.display = "flex";   
+    }
+    
+    if(mainInputValue != "Fablab") {
+        document.getElementById('main-search').classList.remove('dropdown__module--dropped');
+        document.getElementById('main-dropdown').style.display = "none";   
+    }
 });
