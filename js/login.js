df37f92bcf14072153b6bcae980e057ecc8f37d5
Toreristen <82028481+Toreristen@users.noreply.github.com>
2021-09-21 13:42:21 +0200

    login js - fejlfinding

--- js/login.js
+++ js/login.js
@@ -1,6 +1,11 @@
-let userName = ["Danni", "Tore", "Hedinn", "Simon", "Arton"];
+console.log("js virker")
+
+let name = ["Danni", "Tore", "Hedinn", "Simon", "Arton"];
 let passWord = "123";
 
 function login() {
+  let userName = document.getElementById('username').value;
+  let passWord = document.getElementById('password').value;
 
+  document.getElementById('test').textContent = "virker"
 }
