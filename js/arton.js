8099521acecf8babb4f3a2898cb1603e22365f11
TheRealArtonn <61601482+TheRealArtonn@users.noreply.github.com>
2021-09-22 13:23:41 +0200

    html css

--- js/arton.js
+++ js/arton.js
@@ -1,25 +1,26 @@
 const mainInput = document.getElementById('main-input');
-
-
-document.getElementById('main-overlay').style.display = "none";
-
+const mainSearch = document.getElementById('main-search');
+const mainDropdown = document.getElementById('main-dropdown');
+const mainOverlay = document.getElementById('main-overlay');
+                                           
+mainOverlay.style.display = "none";
 document.getElementById('page-input').addEventListener('click', function(e) {
-    document.getElementById('main-overlay').style.display = "block";
+    mainOverlay.style.display = "block";
 });
 
 mainInput.addEventListener('input', function(e) {
     
     let mainInputValue = mainInput.value;
     
-    console.log(mainInputValue);
+    let test = mainInputValue.startsWith("F");
+    console.log(test);
     
-    if(mainInputValue == "Fablab") {
-        document.getElementById('main-search').classList.add('dropdown__module--dropped');
-        document.getElementById('main-dropdown').style.display = "flex";   
-    }
     
-    if(mainInputValue != "Fablab") {
-        document.getElementById('main-search').classList.remove('dropdown__module--dropped');
-        document.getElementById('main-dropdown').style.display = "none";   
+    if(mainInputValue.startsWith("F")) {
+        mainSearch.classList.add('dropdown__module--dropped');
+        mainDropdown.style.display = "flex";   
+    } else {
+        mainSearch.classList.remove('dropdown__module--dropped');
+        mainDropdown.style.display = "none";   
     }
 });
