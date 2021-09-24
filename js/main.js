f244ee65afa93644bdaa6baeca7f153fb4deff09
TheRealArtonn <61601482+TheRealArtonn@users.noreply.github.com>
2021-09-24 12:06:11 +0200

    kort

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([-41.2858, 174.78682], 14);
+mapLink = 
+'<a href="http://openstreetmap.org">OpenStreetMap</a>';
+L.tileLayer(
+'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+attribution: '&copy; ' + mapLink + ' Contributors',
+maxZoom: 18,
+}).addTo(map);
